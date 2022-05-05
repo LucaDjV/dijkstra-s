41ad3c3764e4ea30382640cd52f463f2026a3b33
LucaDjV <103370274+LucaDjV@users.noreply.github.com>
2022-05-05 10:17:32 +0100

    Update Dijkstra'sAlg.cs

--- Dijkstra'sAlg.cs
+++ Dijkstra'sAlg.cs
@@ -1,216 +1,269 @@
 using System;
 using System.Collections.Generic;
 
-namespace djikstraAlg
+namespace BinaryTreesMethods
 {
-    class Program
+    public class BinaryTreeWithJagged
     {
-        public static int dim; //caching array dimensions
-        static void Main(string[] args)
+        public TreeNode root;
+        public TreeNode[][] tempBinaryTree;
+        public BinaryTreeWithJagged(int[] binTree)
         {
-            int[,] adjacencyMat = {
-                { 0, 6, 8, 10, 0},
-                { 6, 0, 5, 0, 10},
-                { 8, 5, 0, 5, 9},
-                { 10, 0, 5, 0, 6},
-                { 0, 10, 9, 6, 0},
-            }; //initialise an adjacency matrix to represent a graph
-            dim = adjacencyMat.GetLength(0);
-            Dictionary<char, int> distances = dijkstra.forwardPass(adjacencyMat);
-            foreach (var s in distances)
+            tempBinaryTree = CreateTree(binTree);
+            int level = GetDepth(binTree);
+            for (int depth = 0; depth < level - 1; depth++)
             {
-                Console.WriteLine("node " + s.Key + " has distance from A of " + s.Value);
-            }
-            Console.ReadKey();
-        }
-    }
-    class dijkstra
-    {
-        public static Dictionary<char, int> forwardPass(int[,] adjacencyMat)
-        {
-            //creates a priority queue to store the distance
-            //values of each node in order while they are
-            //still in the process of being found
-            PriorityQueue labels = new PriorityQueue(adjacencyMat.GetLength(0) + 10);
-
-            //creates a dictionary to pass the node and
-            //distance values to return
-            Dictionary<char, int> result = new Dictionary<char, int>();
-
-            //creates a list of visited nodes
-            List<int> visited = new List<int>();
-
-            //enqueues the 1st node
-            labels.Enqueue(0, 0);
-            priorityItem tempOut = labels.Dequeue();
-            visited.Add(tempOut.Node);
-
-            //changes the integer value to a UTF capital letter
-            char keyOut = (char)(65 + tempOut.Node);
-            result.Add(keyOut, tempOut.Priority);
-            for (int i = 1; i < adjacencyMat.GetLength(1); i++)
-            {
-                //conditional to check if there is a connection and
-                //if the minimum distance has not already been found
-                int currentNode = adjacencyMat[tempOut.Node, i];
-                if (currentNode != 0 && !visited.Contains(i))
+                for (int i = 0; i < tempBinaryTree[depth].Length; i++)
                 {
-                    //checking if the node is already in the label queue
-                    if (labels.Contains(i))
-                    {
-                        //if the new distance is less than the old one
-                        if (tempOut.Priority + currentNode < labels.GetPriority(i))
-                        {
-                            //changing the distance to the new, shorter distance
-                            labels.changePriority(i, tempOut.Priority + currentNode);
-                        }
-                    }
-                    else
-                    {
-                        //adding the new node and distance to the queue
-                        labels.Enqueue(i, tempOut.Priority + currentNode);
-                    }
+                    AssignChildNodes(depth, i);
                 }
             }
-
-            //loops until there are no more nodes to check
-            while (labels.GetLength() > 0)
+            DisplayTree(tempBinaryTree);
+            root = tempBinaryTree[0][0];
+        }
+        public BinaryTreeWithJagged(TreeNode Root)
+        {
+            root = Root;
+        }
+        public int GetDepth(int[] binTree)
+        {
+            int level = 0;
+            int lengthRemaing = binTree.Length - 1;
+            while (lengthRemaing > 0)
             {
-                //dequeues the item with the shortest distance and
-                //adds it to the visited list and distance dictionary
-                priorityItem temp = labels.Dequeue();
-                visited.Add(temp.Node);
-                //changes the integer value to a UTF capital letter
-                char key = (char)(65 + temp.Node);
-                result.Add(key, temp.Priority);
-
-                //loops through the child nodes of the node we just dequeued
-                for (int i = 0; i < adjacencyMat.GetLength(1); i++)
+                for (int i = 0; i < Math.Pow(2, level); i++)
                 {
-                    //conditional to check if there is a connection and
-                    //if the minimum distance has not already been found
-                    int currentNode = adjacencyMat[temp.Node, i];
-                    bool visitedBool = visited.Contains(i);
-                    if (currentNode != 0 && !visitedBool)
+                    lengthRemaing--;
+                }
+                level++;
+            }
+            return level;
+        }
+        public void DisplayTree(TreeNode[][] tempBinaryTree)
+        {
+            foreach (TreeNode[] a in tempBinaryTree)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    Console.Write(" " + a[i].val);
+                }
+                Console.WriteLine();
+            }
+        }
+        public void AssignChildNodes(int row, int col)
+        {
+            int nextRowCount = 2 * col;
+            TreeNode leftNode = tempBinaryTree[row + 1][nextRowCount];
+            TreeNode rightNode = tempBinaryTree[row + 1][nextRowCount + 1];
+
+            tempBinaryTree[row][col].left = leftNode;
+            /*Console.WriteLine("object refs equal = " + object.ReferenceEquals(leftNode, tempBinaryTree[row][col].left));*/
+            tempBinaryTree[row][col].right = rightNode;
+        }
+        public TreeNode[][] CreateTree(int[] binTree)
+        {
+            TreeNode rootVal = new TreeNode(binTree[0]);
+            TreeNode[] rootTemp = { rootVal }; // initialised an array of length 1 to store the root so we can add that the the final array
+            int level = GetDepth(binTree); // getting the amount of levels of the binary tree
+            TreeNode[][] tempBinaryTree = new TreeNode[level][]; // declaring a jagged array to store the levels with their nodes in order
+            double pointer = 0;
+            for (int levelTemp = 0; levelTemp < level; levelTemp++)
+            {
+                List<TreeNode> levelList = new List<TreeNode>();
+                double i = 0;
+                while (i < Math.Pow(2, levelTemp))
+                {
+                    try
                     {
-                        //checking if the node is already in the label queue
-                        if (labels.Contains(i))
+                        TreeNode temp = new TreeNode(binTree[(int)(pointer + i)]);
+                        levelList.Add(temp);
+
+                        i++;
+                    }
+                    catch
+                    {
+                        TreeNode temp = new TreeNode(-1);
+                        levelList.Add(temp);
+                    }
+                }
+                pointer += Math.Pow(2, levelTemp);
+                tempBinaryTree[levelTemp] = levelList.ToArray();
+            }
+            return tempBinaryTree;
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class InvertBinTree
+    {
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            TreeNode left = InvertTree(root.left);
+            TreeNode right = InvertTree(root.right);
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            return root;
+        }
+    } //perfection
+    public class MaximumPath
+    {
+        public static int MaxPathSum(TreeNode root)
+        {
+            int maxPath = 0;
+            List<TreeNode> vistited = new List<TreeNode>();
+            List<int> pathVals = new List<int>();
+            int pathCount = 0;
+            dfs(root, vistited, pathVals, pathCount);
+            foreach (var path in pathVals)
+            {
+                if (path > maxPath)
+                {
+                    maxPath = path;
+                }
+            }
+            return maxPath;
+
+        }
+        /*        public static bool dfs(TreeNode root, List<TreeNode> visited, List<int> pathVals, int pathCount, bool backtracking)
+                {
+                    if (backtracking)
+                    {
+                        pathCount--;
+                    }
+                    if ((root.left != null) || (root.left != null))
+                    {
+                        if (!visited.Contains(root.left) && root.left != null)
                         {
-                            //if the new distance is less than the old one
-                            if (temp.Priority + currentNode < labels.GetPriority(i))
+                            visited.Add(root.left);
+                            backtracking = dfs(root.left, visited, pathVals, pathCount, false);
+                            if (!visited.Contains(root.right))
                             {
-                                if (currentNode != 0)
-                                {
-                                    //changing the distance to the new, shorter distance
-                                    labels.changePriority(i, temp.Priority + currentNode);
-                                }
+                                backtracking = dfs(root.right, visited, pathVals, pathCount + 1, false);
+                            }
+                            else
+                            {
+                                pathVals[pathCount] += root.val;
+                                return true;
+                            }
+                        }
+                        else if (!visited.Contains(root.right) && root.right != null)
+                        {
+                            visited.Add(root.right);
+                            backtracking = dfs(root.right, visited, pathVals, pathCount, false);
+                            if (!visited.Contains(root.left))
+                            {
+                                backtracking = dfs(root.left, visited, pathVals, pathCount + 1, false);
+                            }
+                            else
+                            {
+                                pathVals[pathCount] += root.val;
+                                return true;
                             }
                         }
                         else
                         {
-                            if (currentNode != 0)
-                            {
-                                //adding the new node and distance to the queue
-                                labels.Enqueue(i, temp.Priority + currentNode);
-                            }
+                            return true;
                         }
                     }
+                    else { return true; }
+                }*/
+        public static void dfs(TreeNode root, List<TreeNode> visited, List<int> pathVals, int pathCount)
+        {
+            if (pathCount == 0)
+            {
+                pathVals.Add(root.val);
+                visited.Add(root);
+            }
+            else if (!visited.Contains(root))
+            {
+                if (pathCount < pathVals.Count - 1)
+                {
+                    pathVals.Insert(pathCount, pathVals[pathCount - 1] + root.val);
+                    visited.Add(root);
+                }
+                else
+                {
+                    pathVals.Add(pathVals[pathCount - 1] + root.val);
+                    visited.Add(root);
                 }
             }
-            return result;
-        }
-    }
-    public struct priorityItem //structure for the items
-    {
-        public int Node;
-        public int Priority;
-        public priorityItem(int ValueIn, int PriorityIn)
-        {
-            Node = ValueIn;
-            Priority = PriorityIn;
-        }
-    }
-    public class PriorityQueue
-    {
-        int rear;
-        int front;
-        priorityItem[] contents; //array of structures to store items
-        int itemCount;
-        public PriorityQueue(int size)
-        {
-            contents = new priorityItem[size];
-            rear = -1;
-            front = 0;
-            itemCount = 0;
-        }
-        public int GetPriority(int item)
-        {
-            return contents[item].Priority;
-        }
-        public void changePriority(int item, int newPriority)
-        {
-            contents[item].Priority = newPriority;
-            int numsToChange = itemCount;
-            while (numsToChange > 0)
+            if (root.right != null && !visited.Contains(root.right))
             {
-                for (int i = front + numsToChange - 1; i > 0; i--)
+                dfs(root.right, visited, pathVals, pathCount + 1);
+                if (root.left != null && !visited.Contains(root.left))
                 {
-                    if (contents[i].Priority < contents[i - 1].Priority)
+                    dfs(root.left, visited, pathVals, pathCount + 1);
+                }
+            }
+            else if (root.left != null && !visited.Contains(root.left))
+            {
+                dfs(root.left, visited, pathVals, pathCount + 1);
+                if (root.right != null && !visited.Contains(root.right))
+                {
+                    dfs(root.right, visited, pathVals, pathCount + 1);
+                }
+            }
+            if (root.left != null && root.right != null)
+            {
+                if (pathCount > 0)
+                {
+                    if (pathVals[pathCount + 1] + pathVals[pathCount + 2] - pathVals[pathCount - 1] * 2 - root.val > pathVals[pathCount + 1])
                     {
-                        priorityItem temp = contents[i];
-                        contents[i] = contents[i - 1];
-                        contents[i - 1] = temp;
+                        pathVals.Add(pathVals[pathCount + 1] + pathVals[pathCount + 2] - pathVals[pathCount - 1] * 2 - root.val);
                     }
                 }
-                numsToChange--;
-            }
-        }
-        public bool Contains(int item)
-        {
-            for (int i = front; i <= rear; i++)
-            {
-                if (contents[i].Node == item)
+                else
                 {
-                    return true;
-                }
-            }
-            return false;
-        }
-        public priorityItem Peek()
-        {
-            if (itemCount == 0) { throw new Exception("no items in queue"); }
-            return contents[front];
-        }
-        public int GetLength()
-        {
-            return itemCount;
-        }
-        public void Enqueue(int input, int priority)
-        {
-            rear++;
-            itemCount++;
-            contents[rear] = new priorityItem(input, priority);
-            int numsToChange = itemCount;
-            while (numsToChange > 0)
-            {
-                for (int i = front + numsToChange - 1; i > 0; i--)
-                {
-                    if (contents[i].Priority < contents[i - 1].Priority)
+                    if (pathVals[pathCount + 1] + pathVals[pathCount + 2] - root.val > pathVals[pathCount + 1])
                     {
-                        priorityItem temp = contents[i];
-                        contents[i] = contents[i - 1];
-                        contents[i - 1] = temp;
+                        pathVals.Add(pathVals[pathCount + 1] + pathVals[pathCount + 2]);
                     }
                 }
-                numsToChange--;
+            }
+        } //doesnt work quite right. passed 25/94 tests on leetcode so works in a lot of cases...
+
+        public static int MaxSum(TreeNode node, List<int> pathVals)
+        {
+            int[] temp = { MaxVoid(node.left, pathVals), MaxVoid(node.right, pathVals) };
+            int totSubTree = temp[0] + temp[1];
+            if (temp[0] > temp[1])
+            {
+                pathVals.Add(temp[0]);
+            }
+            else
+            {
+                pathVals.Add(temp[1]);
+            }
+            if(pathVals[pathVals.Count] < totSubTree)
+            {
+                pathVals.Add(totSubTree);
             }
         }
-        public priorityItem Dequeue()
+        public static void MaxVoid(TreeNode node, List<int> pathVals)
         {
-            if (itemCount == 0) { throw new Exception("no items in queue"); }
-            front++;
-            itemCount--;
-            return contents[front - 1];
+            MaxVoid(node.left, pathVals);
+            MaxVoid(node.right, pathVals);
+            int temp = pathVals[pathVals.Count - 1];
+            int totSubTree = temp[0] + temp[1];
+            if (temp < totSubTree)
+            {
+                pathVals.Add(totSubTree);
+            }
         }
-    }
+    }  
 }
